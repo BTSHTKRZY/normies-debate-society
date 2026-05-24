05ec35d0380f3485e74998b13a68523b495282b5
BTSHTKRZY <nlupulkit@gmail.com>
2026-05-24 22:38:35 +0530

    Update leaderboard.js

--- api/leaderboard.js
+++ api/leaderboard.js
@@ -13,7 +13,7 @@ module.exports = async function handler(req, res) {
   if (req.method === 'OPTIONS') { res.status(200).end(); return; }
 
   try {
-    const ids = await redis.lrange('debate:index', 0, 99);
+    const ids = await redis.lrange('debate:index', 0, 19);
     const agentIds = new Set();
 
     for (const id of ids) {
